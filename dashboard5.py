9e717eca257ec18a031826f16453a1044d2943c6
Ramansh25 <146805645+Ramansh25@users.noreply.github.com>
2024-07-02 19:19:30 +0530

    Update dashboard5.py

--- dashboard5.py
+++ dashboard5.py
@@ -6,9 +6,14 @@
 
 
 import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
-from wordcloud import WordCloud
+
+try:
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    from wordcloud import WordCloud
+except ModuleNotFoundError as e:
+    st.error(f"Error: {e}")
+    st.stop()    
 
 # Load the CSV file
 csv_filename = 'YouTube_Comments_Sentiment_KsEoGDgKdwQ_20240701014137.csv'
